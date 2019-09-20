e29b4421befc2533ec694620dac1d0e9887a717a
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2019-09-20 12:34:20 +0200

    Ported unit tests from WireGuard

--- tests/tests.rs
+++ tests/tests.rs
@@ -186,3 +186,166 @@ fn issue_7() {
 
     println!("len: {}", table.len());
 }
+
+#[test]
+fn ipv4_tests() {
+    let a = 1;
+    let b = 2;
+    let c = 3;
+    let d = 4;
+    let e = 5;
+    let g = 6;
+    let h = 7;
+
+    let mut table: IpLookupTable<Ipv4Addr, usize> = IpLookupTable::new();
+
+    macro_rules! insert {
+        ($v:expr, $o1:expr, $o2:expr, $o3:expr, $o4:expr, $cidr:expr) => {
+            table.insert(Ipv4Addr::new($o1, $o2, $o3, $o4), $cidr, $v);
+        };
+    }
+
+    macro_rules! assert_match {
+        ($v:expr, $o1:expr, $o2:expr, $o3:expr, $o4:expr) => {
+            let res = table
+                .longest_match(Ipv4Addr::new($o1, $o2, $o3, $o4))
+                .unwrap();
+            assert_eq!(*res.2, $v);
+        };
+    }
+
+    macro_rules! assert_not_match {
+        ($v:expr, $o1:expr, $o2:expr, $o3:expr, $o4:expr) => {
+            let res = table
+                .longest_match(Ipv4Addr::new($o1, $o2, $o3, $o4))
+                .unwrap();
+            assert_ne!(*res.2, $v);
+        };
+    }
+
+    macro_rules! remove_by_value {
+        ($v: expr) => {
+            let mut remove = vec![];
+            for (ip, cidr, v) in table.iter_mut() {
+                if *v == $v {
+                    remove.push((ip, cidr));
+                }
+            }
+            for (ip, cidr) in remove {
+                table.remove(ip, cidr);
+            }
+        };
+    }
+
+    insert!(a, 192, 168, 4, 0, 24);
+    insert!(b, 192, 168, 4, 4, 32);
+    insert!(c, 192, 168, 0, 0, 16);
+    insert!(d, 192, 95, 5, 64, 27);
+    insert!(c, 192, 95, 5, 65, 27);
+    insert!(e, 0, 0, 0, 0, 0);
+    insert!(g, 64, 15, 112, 0, 20);
+    insert!(h, 64, 15, 123, 128, 25);
+    insert!(a, 10, 0, 0, 0, 25);
+    insert!(b, 10, 0, 0, 128, 25);
+    insert!(a, 10, 1, 0, 0, 30);
+    insert!(b, 10, 1, 0, 4, 30);
+    insert!(c, 10, 1, 0, 8, 29);
+    insert!(d, 10, 1, 0, 16, 29);
+
+    assert_match!(a, 192, 168, 4, 20);
+    assert_match!(a, 192, 168, 4, 0);
+    assert_match!(b, 192, 168, 4, 4);
+    assert_match!(c, 192, 168, 200, 182);
+    assert_match!(c, 192, 95, 5, 68);
+    assert_match!(e, 192, 95, 5, 96);
+    assert_match!(g, 64, 15, 116, 26);
+    assert_match!(g, 64, 15, 127, 3);
+
+    insert!(a, 1, 0, 0, 0, 32);
+    insert!(a, 64, 0, 0, 0, 32);
+    insert!(a, 128, 0, 0, 0, 32);
+    insert!(a, 192, 0, 0, 0, 32);
+    insert!(a, 255, 0, 0, 0, 32);
+
+    assert_match!(a, 1, 0, 0, 0);
+    assert_match!(a, 64, 0, 0, 0);
+    assert_match!(a, 128, 0, 0, 0);
+    assert_match!(a, 192, 0, 0, 0);
+    assert_match!(a, 255, 0, 0, 0);
+
+    remove_by_value!(a);
+
+    assert_not_match!(a, 1, 0, 0, 0);
+    assert_not_match!(a, 64, 0, 0, 0);
+    assert_not_match!(a, 128, 0, 0, 0);
+    assert_not_match!(a, 192, 0, 0, 0);
+    assert_not_match!(a, 255, 0, 0, 0);
+}
+
+#[test]
+fn ipv6_tests() {
+    let a = 1;
+    let b = 2;
+    let c = 3;
+    let d = 4;
+    let e = 5;
+    let f = 6;
+    let g = 7;
+    let h = 8;
+
+    let mut table: IpLookupTable<Ipv6Addr, usize> = IpLookupTable::new();
+
+    macro_rules! ipv6 {
+        ($o1:expr, $o2:expr, $o3:expr, $o4:expr) => {{
+            let o1: u32 = $o1;
+            let o2: u32 = $o2;
+            let o3: u32 = $o3;
+            let o4: u32 = $o4;
+            Ipv6Addr::new(
+                (o1 >> 16) as u16,
+                (o1 & 0xffff) as u16,
+                (o2 >> 16) as u16,
+                (o2 & 0xffff) as u16,
+                (o3 >> 16) as u16,
+                (o3 & 0xffff) as u16,
+                (o4 >> 16) as u16,
+                (o4 & 0xffff) as u16,
+            )
+        }};
+    }
+
+    macro_rules! insert {
+        ($v:expr, $o1:expr, $o2:expr, $o3:expr, $o4:expr, $cidr:expr) => {
+            table.insert(ipv6!($o1, $o2, $o3, $o4), $cidr, $v);
+        };
+    }
+
+    macro_rules! assert_match {
+        ($v:expr, $o1:expr, $o2:expr, $o3:expr, $o4:expr) => {
+            let res = table.longest_match(ipv6!($o1, $o2, $o3, $o4)).unwrap();
+            assert_eq!(*res.2, $v);
+        };
+    }
+
+    insert!(d, 0x26075300, 0x60006b00, 0, 0xc05f0543, 128);
+    insert!(c, 0x26075300, 0x60006b00, 0, 0, 64);
+    insert!(e, 0, 0, 0, 0, 0);
+    insert!(f, 0, 0, 0, 0, 0);
+    insert!(g, 0x24046800, 0, 0, 0, 32);
+    insert!(h, 0x24046800, 0x40040800, 0xdeadbeef, 0xdeadbeef, 64);
+    insert!(a, 0x24046800, 0x40040800, 0xdeadbeef, 0xdeadbeef, 128);
+    insert!(c, 0x24446800, 0x40e40800, 0xdeaebeef, 0xdefbeef, 128);
+    insert!(b, 0x24446800, 0xf0e40800, 0xeeaebeef, 0, 98);
+
+    assert_match!(d, 0x26075300, 0x60006b00, 0, 0xc05f0543);
+    assert_match!(c, 0x26075300, 0x60006b00, 0, 0xc02e01ee);
+    assert_match!(f, 0x26075300, 0x60006b01, 0, 0);
+    assert_match!(g, 0x24046800, 0x40040806, 0, 0x1006);
+    assert_match!(g, 0x24046800, 0x40040806, 0x1234, 0x5678);
+    assert_match!(f, 0x240467ff, 0x40040806, 0x1234, 0x5678);
+    assert_match!(f, 0x24046801, 0x40040806, 0x1234, 0x5678);
+    assert_match!(h, 0x24046800, 0x40040800, 0x1234, 0x5678);
+    assert_match!(h, 0x24046800, 0x40040800, 0, 0);
+    assert_match!(h, 0x24046800, 0x40040800, 0x10101010, 0x10101010);
+    assert_match!(a, 0x24046800, 0x40040800, 0xdeadbeef, 0xdeadbeef);
+}
